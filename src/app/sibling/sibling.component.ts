b4218580dd87b6ff27f1d66c7743ed9d877a285e
Nelaxshan UCA <nelaxshan@unicornconnectedapps.com>
2025-02-12 16:32:10 +0530

    Component Created

---
+++ src/app/sibling/sibling.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sibling',
+  standalone: false,
+  templateUrl: './sibling.component.html',
+  styleUrl: './sibling.component.css'
+})
+export class SiblingComponent {
+
+}
